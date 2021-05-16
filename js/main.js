934a8f6d99a25804c41c12528aa3397027c43be0
Erick Ruiz <erickruiz29@gmail.com>
2021-05-16 08:44:53 -0700

    style kanban card

--- js/main.js
+++ js/main.js
@@ -8,6 +8,16 @@ function mainFunction() {
       newCard.classList.add("kanban-card")
       newCard.innerText = "New Card"
 
+      var titleLabel = document.createElement("label")
+      titleLabel.innerText = "Title"
+      titleLabel.classList.add("kanban-card-title")
+      newCard.append(titleLabel)
+
+      var titleInput = document.createElement("input")
+      titleInput.type = "text"
+      titleInput.placeholder = "Card Title"
+      titleInput.classList.add("kanban-card-input")
+
       if (thisEl instanceof HTMLDivElement) {
         thisEl.before(newCard)
       }
